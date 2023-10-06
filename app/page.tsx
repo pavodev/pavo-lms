42adde0b46842544db19727a453f9a63a5ac7820
Ivan Pavic <ivan96jp@gmail.com>
2023-10-06 16:26:18 +0200

    Added prettier and button component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-import Image from 'next/image'
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/mode-toggle";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <p className='text-3xl font-medium text-sky-700'>Hello World</p>
-  )
+    <div>
+      <p className="text-3xl font-medium text-sky-700">Hello World</p>
+      <ModeToggle></ModeToggle>
+    </div>
+  );
 }
