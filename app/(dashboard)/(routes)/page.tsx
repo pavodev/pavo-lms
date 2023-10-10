6361caf63cd3a720d8a4339277993520129955ef
Ivan Pavic <ivan96jp@gmail.com>
2023-10-10 16:38:40 +0200

    fix

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <p className="text-3xl font-medium text-sky-700">Hello World</p>
+      <p>This is a protected page</p>
       <ModeToggle></ModeToggle>
     </div>
   );
