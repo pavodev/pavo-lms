8391c031484617b86611e4123c3e53723506dabe
Ivan Pavic <ivan96jp@gmail.com>
2023-10-10 17:01:36 +0200

    added clerk auth

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,11 +1,13 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
 import { ModeToggle } from "@/components/ui/mode-toggle";
-import Image from "next/image";
+import { UserButton } from "@clerk/clerk-react";
 
 export default function Home() {
   return (
     <div>
       <p>This is a protected page</p>
+      <UserButton afterSignOutUrl="/" />
       <ModeToggle></ModeToggle>
     </div>
   );
---
+++ app/test/page.tsx
@@ -0,0 +1,5 @@
+const TestPage = () => {
+  return <div>Unprotected</div>;
+};
+
+export default TestPage;
