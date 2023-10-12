ed670bbb92f69d79847f680d5cc6a0a55a047e0f
Ivan Pavic <ivan96jp@gmail.com>
2023-10-12 14:33:18 +0200

    implemented dynamic layout

--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -4,7 +4,7 @@ import SidebarRoutes from "./sidebar-routes";
 const Sidebar = () => {
   return (
     <div className="h-full border-r flex flex-col overflow-y-auto bg-white dark:bg-zinc-950 shadow-sm">
-      <div className="p-6">
+      <div className="p-6 flex items-center justify-center">
         <Logo />
       </div>
       <div className="flex flex-col w-full">
